6f4ca16644ada8820ea9c7794e3fc11391f07bf1
Zhang Xi <39937546+xzhangi@users.noreply.github.com>
2018-07-24 16:49:32 +0800

    Initial Commit 2

---
+++ CodeIgniter/application/controllers/Home.php
@@ -0,0 +1,29 @@
+<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+
+	class Home extends CI_Controller {
+
+		public function __construct()
+		{
+			parent::__construct();
+			$this->load->helper('url');
+			$this->load->database();
+		}
+		 
+		public function index()
+		{
+			// Load model
+			$this->load->model('Lockermodel');
+			//Call any required model functions
+			$alllockers = $this->Lockermodel->get_locker_list_all();
+			$data['lockerlist'] = $alllockers;
+			//Load the view
+			$this->load->view('Main-Home', $data);
+		}
+		
+/* 		public function dashboard()
+		{
+			$this->load->view('Dashboard');
+		} */
+	}
+?>
---
+++ CodeIgniter/application/views/SAMPLE PAGES FOR ADMIN/README.txt
@@ -0,0 +1,2 @@
+This folder contains the sample pages for the admin page, if you need the page, copy it out and paste it in the view folder
+1. change file extension from .html to .php
