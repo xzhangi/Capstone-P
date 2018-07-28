cb247fb128c6c37b200d0c65eb9e81e2c77cb692
janessatng <39946612+janessatng@users.noreply.github.com>
2018-07-29 03:17:50 +0800

    janessa

---
+++ CodeIgniter/assets/Default-BS/lockers/medium.php
@@ -0,0 +1,9 @@
+<div class="medium-img">
+	<img src="assets/default-BS/img/lockers/medium2.png" alt="">
+</div>
+<div class="cbp-l-member-info">
+	<div class="cbp-l-member-name">Medium Locker</div>
+	<div class="cbp-l-member-position">20 x 8 x 6 inches</div>
+	<div class="cbp-l-member-desc">
+	</div>
+</div>
---
+++ CodeIgniter/assets/Default-BS/lockers/small.php
@@ -0,0 +1,9 @@
+<div class="small-img">
+	<img src="assets/default-BS/img/lockers/small2.png" alt="">
+</div>
+<div class="cbp-l-member-info">
+	<div class="cbp-l-member-name">Small Locker</div>
+	<div class="cbp-l-member-position">10 x 5 x 4 inches</div>
+	<div class="cbp-l-member-desc">
+	</div>
+</div>
